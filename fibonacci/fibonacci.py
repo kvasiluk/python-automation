649ef1dfd11beaaff752976b8aed30efa6cc1ea6
Kirill Vasiluk <38853284+kvasiluk@users.noreply.github.com>
2018-08-22 13:18:03 +0300

    Phase 1 python basics (#3)
    * Implement Fibonacci function
    
    * Add tests for Fibonacci function
    
    * Add CircleCI integration
    
    * Change run tests CircleCI step
    
    * Change fibonacci function name; add main method
    
    * Change fibonacci function name; add main method
    
    * Remove extra int parse from fibonacci function

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,5 +1,5 @@
-def fibonacci(n=10):
-    n = int(n)
+def generateFibonacci(length=10):
+    n = int(length)
 
     if n < 0:
         return 0
@@ -8,10 +8,19 @@ def fibonacci(n=10):
 
     seq = [1, 1]
 
-    for i in range(2, int(n)):
+    for i in range(2, n):
         seq.append(seq[i - 2] + seq[i - 1])
 
     return seq
 
 
-print(fibonacci())
+def main():
+    print(generateFibonacci())
+    print(generateFibonacci(20))
+    print(generateFibonacci(1))
+    print(generateFibonacci(0))
+    print(generateFibonacci(-999))
+
+
+if __name__ == "__main__":
+    main()
