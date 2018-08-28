f7e328efcff5029fb60045c0eb427dfcbb2dd6bb
kvasiluk <kvasiluk@lohika.com>
2018-08-28 17:09:53 +0300

    Add http requests module

---
+++ api_tests/common/http_request.py
@@ -0,0 +1,91 @@
+import requests
+from requests.compat import basestring
+
+DEFAULT_HEADER = {'Content-Type': 'application/json'}
+DEFAULT_TIMEOUT = 15
+
+
+class BaseHttp(object):
+    """
+    Main Http interactions module
+    """
+
+    def __init__(self, headers=None, cookies=None):
+        self.session = requests.session()
+        self.session.headers.update(DEFAULT_HEADER)
+        self.base_url = 'http://jira.hillel.it:8080'
+        if headers:
+            self.session.headers.update(headers)
+        if cookies:
+            self.session.cookies.update(cookies)
+
+    def get(self, url_part, headers=None, cookies=None, timeout=DEFAULT_TIMEOUT, **kwargs):
+        """
+        :param url_part: request uri part. Not needed to put full url
+        :param list cookies: custom request cookies
+        :param timeout: custom timeout
+        :param dict headers: custom request headers
+        :rtype: requests.Response
+        """
+
+        url = self._make_url(url_part)
+
+        response = self.session.get(url, cookies=cookies, headers=headers, timeout=timeout, **kwargs)
+
+        return response
+
+    def post(self, url_part, content=None, headers=None, cookies=None, timeout=DEFAULT_TIMEOUT, **kwargs):
+        """
+        :param url_part: request uri part. Not needed to put full url
+        :param content: data to send
+        :param list cookies: custom request cookies
+        :param timeout: custom timeout
+        :param dict headers: custom request headers
+        :rtype: requests.Response
+        """
+
+        url = self._make_url(url_part)
+
+        response = self.session.post(url, data=content, cookies=cookies, headers=headers, timeout=timeout, **kwargs)
+
+        return response
+
+    def put(self, url_part, content=None, headers=None, cookies=None, timeout=DEFAULT_TIMEOUT, **kwargs):
+        """
+        :param url_part: request uri part. Not needed to put full url
+        :param content: data to send
+        :param list cookies: custom request cookies
+        :param timeout: custom timeout
+        :param dict headers: custom request headers
+        :rtype: requests.Response
+        """
+
+        url = self._make_url(url_part)
+
+        response = self.session.put(url, data=content, cookies=cookies, headers=headers, timeout=timeout,**kwargs)
+
+        return response
+
+    def delete(self, url_part, headers=None, cookies=None, timeout=DEFAULT_TIMEOUT, **kwargs):
+        """
+        :param url_part: request uri part. Not needed to put full url
+        :param list cookies: custom request cookies
+        :param timeout: custom timeout
+        :param dict headers: custom request headers
+        :rtype: requests.Response
+        """
+
+        url = self._make_url(url_part)
+
+        response = self.session.delete(url, cookies=cookies, headers=headers, timeout=timeout, **kwargs)
+
+        return response
+
+    def update_auth_cookie(self, response):
+        if response and isinstance(response, basestring):
+            self.session.cookies.update({'JSESSIONID': response})
+        elif response.json()['session']['name'] == 'JSESSIONID':
+            self.session.cookies.update({'JSESSIONID': response.json()['session']['value']})
+
+    def _make_url(self, url_part):
+        return '%s/%s' % (self.base_url, url_part)
--- api_tests/test_login.py
+++ api_tests/test_login.py
@@ -1,9 +1,9 @@
-import requests
 import json
 import pytest
 
-base_url = 'http://jira.hillel.it:8080'
-auth_url = '/rest/auth/1/session'
+from api_tests.common.http_request import BaseHttp
+
+auth_url = 'rest/auth/1/session'
 
 
 @pytest.mark.parametrize("username,password,response_code", [
@@ -12,8 +12,8 @@ auth_url = '/rest/auth/1/session'
     ("Kirill_Vasiluk", "Wrong_Password", 401),
 ])
 def test_login(username, password, response_code):
-    application_json = {"Content-Type": "application/json"}
+    http = BaseHttp()
     payload = json.dumps({'username': username, 'password': password}, ensure_ascii=False)
 
-    login_resp = requests.session().post(base_url + auth_url, data=payload, headers=application_json, timeout=15)
+    login_resp = http.post(auth_url, payload)
     assert login_resp.status_code == response_code
