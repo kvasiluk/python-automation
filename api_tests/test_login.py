5b12ebda231bbed656365fecbcbd8e840440c61a
kvasiluk <kvasiluk@lohika.com>
2018-08-28 16:32:55 +0300

    Add parametrization for login test; remove duplicated tests

--- api_tests/test_login.py
+++ api_tests/test_login.py
@@ -1,35 +1,19 @@
 import requests
 import json
+import pytest
 
 base_url = 'http://jira.hillel.it:8080'
 auth_url = '/rest/auth/1/session'
 
 
-def test_login_positive():
-    username = 'Kirill_Vasiluk'
-    password = 'Kirill_Vasiluk'
+@pytest.mark.parametrize("username,password,response_code", [
+    ("Kirill_Vasiluk", "Kirill_Vasiluk", 200),
+    ("Wrong_Username", "Kirill_Vasiluk", 401),
+    ("Kirill_Vasiluk", "Wrong_Password", 401),
+])
+def test_login(username, password, response_code):
     application_json = {"Content-Type": "application/json"}
     payload = json.dumps({'username': username, 'password': password}, ensure_ascii=False)
 
     login_resp = requests.session().post(base_url + auth_url, data=payload, headers=application_json, timeout=15)
-    assert login_resp.status_code == 200
-
-
-def test_login_negative_wrong_username():
-    username = 'Wrong_Username'
-    password = 'Kirill_Vasiluk'
-    application_json = {"Content-Type": "application/json"}
-    payload = json.dumps({'username': username, 'password': password}, ensure_ascii=False)
-
-    login_resp = requests.session().post(base_url + auth_url, data=payload, headers=application_json, timeout=15)
-    assert login_resp.status_code == 401
-
-
-def test_login_negative_wrong_password():
-    username = 'Kirill_Vasiluk'
-    password = 'Wrong_Password'
-    application_json = {"Content-Type": "application/json"}
-    payload = json.dumps({'username': username, 'password': password}, ensure_ascii=False)
-
-    login_resp = requests.session().post(base_url + auth_url, data=payload, headers=application_json, timeout=15)
-    assert login_resp.status_code == 401
+    assert login_resp.status_code == response_code
