90a6cb74f583e973615f56e1c3d47e0f9656adf8
kvasiluk <kvasiluk@lohika.com>
2018-10-02 20:12:11 +0300

    Change chrome driver install

--- ui_tests/test_prepare_data.py
+++ ui_tests/test_prepare_data.py
@@ -1,9 +1,11 @@
 import pytest
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.mark.nobrowser
 @pytest.mark.prepare
 def test_create(create_issues):
+    ChromeDriverManager().install()
     assert True
 
 
